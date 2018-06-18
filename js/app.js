8096a57a9a49aa7adb98935b5c56096feedf8d7d
Alex Delgado <delgadoalex@icloud.com>
2018-06-18 18:51:16 -0500

    2nd commit - created some data structures and accessed some data

--- js/app.js
+++ js/app.js
@@ -1,2 +1,50 @@
 console.log('JS is working'); 
 
+////// DATATYPES //////
+//For each of the following, write which datatypes you would use to represent the data, and then give a small example of the data structure:
+// 1. Boolean // 
+// 2. String // 
+// 3. Array // 
+// 4. Array // 
+// 5. Object // 
+// 6. Object // 
+
+////////// COMMIT #1 ///////////// 
+
+
+////// TAKE IT EASY ////////
+
+// 1. Make an array that holds all of the colors of the rainbow. // 
+const arr1 = ['blue', 'green', 'red', 'yellow', 'orange', 'purple']; 
+
+// 2. Write code that will access "blue" from the rainbow array. // 
+arr1[0]; 
+console.log(arr1[0]);
+
+// 3. Make an object that is called your name and holds the information about your favorite food, a hobby, the name of the town that you live in currently, and your favorite datatype.
+
+const aboutMe = {
+  name: 'Alex Delgado',
+  favoritefood: 'middle eastern',
+  hobby: 'music production', 
+  town: 'Chicago',
+  datatype: 'Objects',
+}
+
+// 4. Write code that will access your hobby from the object that you just created. //
+console.log(aboutMe['hobby']);
+
+
+///////// COMMIT # 2 /////////// 
+
+
+
+
+
+
+
+
+
+
+
+
